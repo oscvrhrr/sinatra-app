c0e557e7682a5db053a430ec8fdf5a882eabab23
Oscar Herrera <97704887+oscvrhrr@users.noreply.github.com>
2025-01-20 03:05:44 +0000

    made api call and rendered gif

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "dotenv/load"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+
+  erb(:home)
+end
+
+get("/search") do 
+  data = params.fetch("q")
+  response = HTTP.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV.fetch("API_KEY")}&q=#{data}")
+  parsed_data = JSON.parse(response)
+
+  @id = parsed_data["data"][0]["id"]
+
+
+
+  erb(:result)
 end
