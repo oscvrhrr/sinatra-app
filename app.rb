6aea4f0886f76b44db9e05a269280d493d3b9a39
Oscar Herrera <97704887+oscvrhrr@users.noreply.github.com>
2025-01-20 23:42:35 +0000

    styled

--- app.rb
+++ app.rb
@@ -5,19 +5,12 @@ require "dotenv/load"
 require "json"
 
 get("/") do
-
-
-  erb(:home)
-end
-
-get("/search") do 
-  data = params.fetch("q")
+  data = params.fetch("q", "ruby on rails") 
   response = HTTP.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV.fetch("API_KEY")}&q=#{data}")
   parsed_data = JSON.parse(response)
 
+  
   @id = parsed_data["data"][0]["id"]
 
-
-
-  erb(:result)
+  erb(:home)
 end
